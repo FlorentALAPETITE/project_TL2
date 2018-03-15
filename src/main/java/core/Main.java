6c1d157f696e9040aadd44937b0dd67f2b769bf5
Polluux <polluux88@yahoo.fr>
2018-03-15 16:13:41 +0100

    added increment example function in main

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,8 +1,22 @@
 package core;
 
+import transactionalMemory.*;
+
 public class Main {
 	
     public static void main( String[] args ){
    
     }
+
+    void increment(IRegister<Integer> X){
+    	while (!t.isCommited()){
+    		try{
+    			t.begin();
+    			X.write(t, X.read(t) + 1);
+    			t.try_to_commit();
+    		}catch(AbortException e){
+    			e.getMessage();
+    		}
+    	}
+	}
 }
