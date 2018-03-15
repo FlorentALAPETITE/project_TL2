8e7561b78a2dfbab755f22f77f3463e38583a0ef
Polluux <polluux88@yahoo.fr>
2018-03-15 16:20:32 +0100

    Added testing of increment of integer register in main

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -5,7 +5,13 @@ import transactionalMemory.*;
 public class Main {
 	
     public static void main( String[] args ){
-   
+    	System.out.println("=== Testing increment for an Integer Register ===");
+   		Register reg = new Register<Integer>();
+   		System.out.println("Value of reg : "+reg.read());
+   		System.out.println("Incrementing");
+   		increment(reg);
+   		System.out.println("New value of reg : "+reg.read());
+   		System.out.println("=== End ===");
     }
 
     void increment(IRegister<Integer> X){
